518c0e915ebc6d2252f348e814421ca6f6dab93e
OstapMaksymiv <ostaperad@gmail.com>
2025-02-02 16:29:10 +0100

    feat:16:29

--- backend/pages/conversation.page.js
+++ backend/pages/conversation.page.js
@@ -2,5 +2,5 @@ import express from 'express'
 import {getConversations } from '../controllers/conversation.controller.js'
 import protectRoute from '../middleware/protectRoute.js';
 const router = express.Router();
-router.get("/",protectRoute,getConversations)
+router.get("/",getConversations)
 export default router
