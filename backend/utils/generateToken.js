d3f19f357d677847957506ac4438252869cbbcb4
OstapMaksymiv <ostaperad@gmail.com>
2025-02-02 19:37:14 +0100

    feat: there no way to fix issue with token(trouble withrender or safari + chrome on phone)

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -3,11 +3,10 @@ export const generateToken = (userId, res) => {
     console.log('started creating token');
     const age = 1000 * 60 * 60 * 24 * 7;
     const token = jwt.sign({
-        userId,
-        isAdmin:false
+        userId
     },process.env.JWT_SECRET,{expiresIn:age})
     res.cookie("token",token,{
-        // httpOnly: true,
+        httpOnly: true,
         maxAge:age,
         secure: true, // Обов'язково для sameSite: 'None'
         sameSite: 'None'
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -5,12 +5,10 @@ import SideChange from '../../components/SideChange/SideChange'
 import './homeStyles.scss'
 
 const Home = () => {
-  const cookies = document.cookie;
+
   return (
     <div className='flex  home-block  rounded-2xl overflow-hidden bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0' style={{backgroundColor:'rgba(0, 0, 0, 0.5)'}}>
-
         <SideChange/>
-        <p style={{width:'500px'}}>{cookies}</p>
         <Sidebar/>
        <MessageContainer/>
     </div>
